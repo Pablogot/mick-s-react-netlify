12b44e59d82c47a4c88b8928d8ef70c8a8046da3
Pablo Llanos <pablollanos92@gmail.com>
2019-04-07 19:26:20 +1000

    added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,22 @@ module.exports = {
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `public/img/apple-icon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      }, 
+    },
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
