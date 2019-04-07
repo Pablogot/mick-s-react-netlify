f09e856a0e0c37afd876d4e6f1e35450575f912b
Pablo Llanos <pablollanos92@gmail.com>
2019-04-07 19:34:03 +1000

    changed icon url

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `public/img/apple-icon.png`, // This path is relative to the root of the site.
+        icon: `static/img/apple-icon.png`, // This path is relative to the root of the site.
         include_favicon: true, // Include favicon
       }, 
     },
